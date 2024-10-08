90d2f1fb5e358c98d9b664cf3db51c7559149408
jjey67 <145826175+jjey67@users.noreply.github.com>
2024-10-08 16:00:43 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+import * as THREE from 'three';
+
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+scene.add(camera)
+
+
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+renderer.setClearColor(0x272727);
+document.body.appendChild(renderer.domElement);
+
+camera.position.z = 400
+
+
+function animate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+
+}
