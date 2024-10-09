b6c8f0361cf7276b64109a1a3a7df060ac01ee61
nayeliMC26 <villan@merrimack.edu>
2024-10-09 13:35:25 -0400

    Refactor: Created classes to make the game more object-oriented Feat: Created a function which should return a random geometry from a list of geometries

--- ObjectManager.js
+++ ObjectManager.js
@@ -1,5 +1,34 @@
+import { IcosahedronGeometry } from "three";
+
 export class ObjectManager{
     constructor(scene, camera){
-        
+        this.scene = scene;
+        this.camera = camera;
+        this.objects = []
+        this.objectsMax = 200
+
     }
+
+    // function to create random objects (takes random geometry as an input to create random objects)
+    createObjects(){
+
+    }
+
+    // function which is just a list of geometries and randomly returns a geometry 
+    /**
+     * @returns Random geometry
+     */
+    randomGeometries(){
+        var geometries = [
+           new THREE.SphereGeometry(5,32,32), // just some random params for the sake of showing how it works for now
+           new THREE.BoxGeometry(1,1,1),
+           new THREE.OctahedronGeometry(3,0),
+           new THREE.IcosahedronGeometry(3,0)
+        ]
+        /** for the length of the array of geometry types, pick a random number from there and return the number at that index 
+         * this may have to be tweaked later but for now its fine 
+        */
+        return geometries[Math.floor(Math.random() * geometries.length)]
+    }
+
 }
--- main.js
+++ main.js
@@ -1,22 +1,37 @@
 import * as THREE from 'three';
 import { ObjectManager } from './ObjectManager.js';
+class Main {
+    constructor() {
+        // adding scene, camera, renderer, making necessary adjustments
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setAnimationLoop(() => this.animate());
+        // nice gray color to start with :)
+        this.renderer.setClearColor(0x272727);
+        document.body.appendChild(this.renderer.domElement);
+        // setting the initial position of the camera, subject to change
+        this.camera.position.z = 400
+        // creating a new objectManager object 
+        this.ObjectManager = new ObjectManager(this.scene, this.camera);
+        // handles window resizing 
+        window.addEventListener('resize', () => this.onWindowResize, false)
+    }
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-scene.add(camera)
+    animate() {
+        requestAnimationFrame(() => this.animate());
+        this.renderer.render(this.scene, this.camera);
 
-
-var renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setAnimationLoop(animate);
-renderer.setClearColor(0x272727);
-document.body.appendChild(renderer.domElement);
-
-camera.position.z = 400
-
-
-function animate() {
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+    }
+    // defines the function of windowResizing
+    onWindowResize() {
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
 
 }
+
+var game = new Main();
+
