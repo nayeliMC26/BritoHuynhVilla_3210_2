d7dc45bc1fdbc93fb0e57a35385b4015114746dd
huynhwmc <156921205+huynhwmc@users.noreply.github.com>
2024-10-09 12:09:58 -0400

    Create the camera

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+const scene = new THREE.Scene();
+// Create the camera facing forward
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
+// Have a few variables to control the camera
+var xLookAt = 0;
+var yLookAt = 0;
+var zLookAt = 0;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-scene.add(camera)
+camera.position.z = 50;
+camera.lookAt(new THREE.Vector3(xLookAt, yLookAt, zLookAt));
+scene.add(camera);
 
+// const controls = new OrbitControls(camera, renderer.domElement);
 
-var renderer = new THREE.WebGLRenderer({ antialias: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 renderer.setClearColor(0x272727);
 document.body.appendChild(renderer.domElement);
-
-camera.position.z = 400
 
 
 function animate() {
