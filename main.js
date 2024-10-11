f5cf9e675d2621c7678b605d613dd6df5c4c2588
huynhwmc <156921205+huynhwmc@users.noreply.github.com>
2024-10-11 10:26:23 -0400

    Created the front camera moving at a steady velocity

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ class Main {
         this.scene = new THREE.Scene();
         // Create the camera facing forward
         this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-        // camera.lookAt(new THREE.Vector3(0, 0, 0));
-        // scene.add(camera);
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+        this.xLookAt = 0;
+        this.yLookAt = -100;
+        this.zLookAt = 0;
+        // this.scene.add(this.camera);
         
         this.renderer = new THREE.WebGLRenderer({ antialias: true });
         this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -18,7 +21,7 @@ class Main {
         this.renderer.setClearColor(0x272727);
         document.body.appendChild(this.renderer.domElement);
         // setting the initial position of the camera, subject to change
-        this.camera.position.z = 200
+        this.camera.position.z = 200;
         // creating a new objectManager object 
         this.ObjectManager = new ObjectManager(this.scene, this.camera);
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -27,8 +30,11 @@ class Main {
     }
 
     animate() {
+        // Move the camera at a forward steady velocity (for now)
+        this.camera.position.set(this.xLookAt, this.yLookAt += 0.1, this.zLookAt);
+        //this.camera.lookAt(new THREE.Vector3(this.xLookAt, this.yLookAt, this.zLookAt -= 0.1));
         this.renderer.render(this.scene, this.camera);
-
+        this.controls.update();
     }
     // defines the function of windowResizing
     onWindowResize() {
