c804e83917c97373a582fa05f3cd505fc73ca91b
britomartinw <britomartinw@merrimack.edu>
2024-10-20 17:44:55 -0400

    trying ot make the mirror follow the camera

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { ObjectManager } from './ObjectManager.js';
+import { Reflector } from 'three/addons/objects/Reflector.js';
 
 class Main {
     constructor() {
@@ -14,80 +15,55 @@ class Main {
         this.renderer.setAnimationLoop(() => this.animate());
         // nice gray color to start with :)
         this.renderer.setClearColor(0x272727);
+        this.renderer.setScissorTest(true);
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         document.body.appendChild(this.renderer.domElement);
 
         // Group to act as the body of the cameras
 
-        // Object with the required information for each camera
-        this.views = [
-            {
-                camera: "",
-                lookAt: new THREE.Vector3(0, 0, -1),
-                // The parameters of the view port (percetages of the screen)
-                left: 0,
-                bottom: 0,
-                width: 1,
-                height: 1,
-                background: 0x272727
-            },
-            {
-                camera: "",
-                lookAt: new THREE.Vector3(0, 0, 1),
-                // The parameters of the view port (percetages of the screen)
-                left: 0.2,
-                bottom: 0.75,
-                width: 0.6,
-                height: 0.2,
-                background: 0x808080
-            }
+        this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
+        this.camera.translateZ(50);
+        this.scene.add(this.camera);
+
+        var mirrorGeometry = new THREE.PlaneGeometry(1, 1);
+        this.mirror = new Reflector(mirrorGeometry, {
+            clipBias: 0.003,
+            textureWidth: window.innerWidth * window.devicePixelRatio,
+            textureHeight: window.innerHeight * window.devicePixelRatio,
+            // color: 0xb5b5b5
+            color: 0xb5b5b5,
+            doubleSided: true
+
+        });
+        this.mirror.position.set(0, 0, -0.1);
+        this.mirror.translateZ(50);
+        this.mirror.visible = false;
+        this.scene.add(this.mirror);
+
+        this.mirrorBounds = [
+            0.2 * window.innerWidth,
+            0.75 * window.innerHeight,
+            0.6 * window.innerWidth,
+            0.2 * window.innerHeight
         ];
+
         this.cameraBody = new THREE.Group();
-        this.cameraBody.rotateY(Math.PI);
-        this.cameraBody.position.set(-10, 0, 50);
-
-
+        // this.cameraBody.rotateY(-Math.PI / 2)
+        this.cameraBody.add(this.camera, this.mirror);
         this.scene.add(this.cameraBody);
-        // Creating the cameras
-        for (let i = 0; i < this.views.length; i++) {
-            const view = this.views[i];
-            const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
-            camera.lookAt(view.lookAt);
-            // camera.position.copy(view.eye);
-            view.camera = camera;
-            this.scene.add(camera);
-            this.cameraBody.add(view.camera);
-        }
-        // this.cameraBody.add(this.views[1].camera);
-        // this.cameraBody.add(this.views[0].camera);
-
-        this.cameraBody.lookAt(-11, 0, 50);
+        // this.cameraBody.position.z += 50;
 
 
-        var geometry = new THREE.SphereGeometry(5, 32, 32);
-        var material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
-        var sphere = new THREE.Mesh(geometry, material);
-        sphere.position.set(50, 10, 0);
-
-        var geometry2 = new THREE.SphereGeometry(5, 32, 32);
-        var material2 = new THREE.MeshBasicMaterial({ color: 0xff00ff });
-        var sphere2 = new THREE.Mesh(geometry2, material2);
-        sphere2.position.set(-50, 10, 0);
-
-        this.scene.add(sphere);
-        this.scene.add(sphere2);
-
-        const axesHelper = new THREE.AxesHelper(5);
-        this.scene.add(axesHelper);
-
-        // this.controls = new OrbitControls(this.views[0].camera, this.renderer.domElement);
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         // creating a new objectManager object 
-        this.ObjectManager = new ObjectManager(this.scene, this.views[0].camera);
+        this.ObjectManager = new ObjectManager(this.scene, this.camera);
         // handles window resizing 
 
         window.addEventListener('resize', () => this.onWindowResize(), false)
 
+        this.axis = new THREE.AxesHelper();
+        this.scene.add(this.axis);
         // Used to calculate delta time
         this.clock = new THREE.Clock();
 
@@ -112,54 +88,56 @@ class Main {
     }
 
     animate() {
+        this.controls.update();
         const speed = 20;
         const deltaTime = this.clock.getDelta() * speed;
         // Move the camera at a slow, forward steady velocity using delta time
-        for (let i = 0; i < this.views.length; i++) {
-            // Picking a camera to work with
-            const view = this.views[i];
-            const camera = view.camera;
+        this.renderer.setScissor(0, 0, window.innerWidth, window.innerHeight);
+        this.renderer.render(this.scene, this.camera);
+        this.mirror.visible = true;
+        this.renderer.setScissor(...this.mirrorBounds);
+        this.renderer.render(this.scene, this.camera);
+        this.mirror.visible = false;
+        this.mirror.position.copy(this.camera.position);
+        var cameraDirection = new THREE.Vector3();
+        this.camera.getWorldDirection(cameraDirection);
+        this.mirror.translateOnAxis(cameraDirection, 0.1)
+        var cameraRotation = new THREE.Euler();
+        this.camera.rotation.copy(cameraRotation);
+        console.log("Bef: ", cameraRotation);
+        cameraRotation.x *= -1;
+        cameraRotation.y *= -1;
+        cameraRotation.z *= -1;
+        console.log("Af: ", cameraRotation);
+        this.mirror.setRotationFromEuler(cameraRotation);
+        console.log("Cam: ", this.camera.rotation);
+        console.log("Mir: ", this.camera.rotation);
+        // console.log("Pos: ", this.camera.position);
+        // console.log("Rot: ", this.camera.rotation);
 
-            // Parameters of the view port
-            const left = Math.floor(view.left * window.innerWidth);
-            const bottom = Math.floor(view.bottom * window.innerHeight);
-            const width = Math.floor(view.width * window.innerWidth);
-            const height = Math.floor(view.height * window.innerHeight);
-
-            // Setting the view port
-            this.renderer.setScissor(left, bottom, width, height);
-            this.renderer.setScissorTest(true);
-            this.renderer.setClearColor(view.background);
-
-
-            // Updating the camera and renderer
-            const deltaTime = this.clock.getDelta();
-            const speed = 20;
-            // camera.position.z -= (deltaTime * speed);
-            // camera.lookAt.z -= (deltaTime * speed);
-
-            this.renderer.render(this.scene, camera);
-        }
-        this.cameraBody.position.z -= deltaTime
+        // this.cameraBody.position.z -= deltaTime
 
         // Moves all the objects in a random linear direction
-        this.ObjectManager.drifting();
+        // this.ObjectManager.drifting();
 
-        this.ObjectManager.transformations();
+        // this.ObjectManager.transformations();
 
-        // Enable blending
-        this.ObjectManager.blend();
+        // // Enable blending
+        // this.ObjectManager.blend();
 
     }
 
     // defines the function of windowResizing
     onWindowResize() {
-        for (let i = 0; i < this.views.length; i++) { // Updating camera aspect for all cameras
-            const view = this.views[i];
-            const camera = view.camera;
-            camera.aspect = Math.floor(view.width * window.innerWidth) / Math.floor(view.height * window.innerHeight);
-            camera.updateProjectionMatrix();
-        }
+
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+        this.mirrorBounds = [
+            0.2 * window.innerWidth,
+            0.75 * window.innerHeight,
+            0.6 * window.innerWidth,
+            0.2 * window.innerHeight
+        ];
         this.renderer.setSize(window.innerWidth, window.innerHeight);
     }
 }
