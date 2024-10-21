5115fadd77017e0429fb33355daf2dc4d71be7f8
britomartinw <britomartinw@merrimack.edu>
2024-10-20 20:43:07 -0400

    Fix: Fixed hidden merge errors

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import * as THREE from 'three';
 import { ObjectManager } from './ObjectManager.js';
-
 import { Reflector } from 'three/addons/objects/Reflector.js';
-
 import { FirstPersonControls } from './controls/FirstPersonControls.js';
 
 
@@ -53,19 +51,14 @@ class Main {
             0.2 * window.innerHeight
         ];
 
-
-        // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-
-        //this.controls = new OrbitControls(this.views[0].camera, this.renderer.domElement);
-        this.controls = new FirstPersonControls(this.views[0].camera, this.renderer.domElement)
+        //this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        this.controls = new FirstPersonControls(this.camera, this.renderer.domElement)
 
         // creating a new objectManager object 
         this.ObjectManager = new ObjectManager(this.scene, this.camera);
-        // handles window resizing 
 
         // Used to calculate delta time
         this.clock = new THREE.Clock();
-
 
         this.ObjectManager.renderStars();
         this.ambientLight = new THREE.AmbientLight(0xffffff, 1);
@@ -85,17 +78,19 @@ class Main {
         this.directionalLight.shadow.camera.near = 1;
         this.directionalLight.shadow.camera.far = 500;
 
+        // handles window resizing 
         window.addEventListener('resize', () => this.onWindowResize(), false);
     }
 
     animate() {
-
         // this.controls.update();
         // The time between animate() calls
         const deltaTime = this.clock.getDelta();
+        const speed = 20;
 
         // Move the camera at a slow, forward steady velocity using delta time
-        this.camera.position.z -= this.cameraSpeed * deltaTime
+        this.controls.update(deltaTime * speed);
+        // this.camera.position.z -= this.cameraSpeed * deltaTime
 
         // Variable that acts as the camera look at direction
         var direction = new THREE.Vector3;
@@ -103,7 +98,6 @@ class Main {
         // Getting the camera look at direction
         this.camera.getWorldDirection(direction);
         direction.normalize();
-
 
         // Updating the mirror to adjust for the new camera posistion
         this.mirror.position.copy(this.camera.position);
@@ -113,15 +107,6 @@ class Main {
         this.mirror.rotateY(Math.PI);
         // Moving it to the near plane of the camera
         this.mirror.translateZ(-0.1);
-            // Updating the camera and renderer
-            const deltaTime = this.clock.getDelta();
-            const speed = 20;
-            // camera.position.z -= (deltaTime * speed);
-            // camera.lookAt.z -= (deltaTime * speed);
-
-         this.controls.update(deltaTime * speed);
-
-
 
         // Moves all the objects in a random linear direction
         this.ObjectManager.drifting();
